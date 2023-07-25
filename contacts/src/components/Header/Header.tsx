b524f0aa2480012606ec4c8a8a6ec64c7f9bbd57
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-25 15:02:32 +1000

    set header font

--- contacts/src/components/Header/Header.tsx
+++ contacts/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="sticky top-0 bg-white z-10">
       <div className="flex flex-col sm:flex-row sm:items-center pr-4 justify-between max-w-[1440px] m-auto">
-        <h1>Contacts</h1>
+        <h1 className="text-[#5f6368] text-xl">Contacts</h1>
       </div>
     </header>
   );
