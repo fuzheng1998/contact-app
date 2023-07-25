36f810c3b5f02d87d48d0a3cbc4d9d879481c168
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-25 16:30:41 +1000

    make ContactList.tsx into table

--- contacts/src/ContactList/ContactList.tsx
+++ contacts/src/ContactList/ContactList.tsx
@@ -8,7 +8,21 @@ function ContactList({
 }: ContactListProps) {
 
   return (
-    <div>
+    <table className="w-full border-separate">
+        <thead className="sticky top-16 z-10 bg-white border-b">
+        <tr>
+            <th className="w-1/2 text-left font-normal py-3 border-b px-4">
+                Name
+            </th>
+            <th className="w-1/2 text-left font-normal py-3 border-b px-4">
+                Phone Number
+            </th>
+            <th className="w-1/2 text-left font-normal py-3 border-b px-4">
+                Email
+            </th>
+        </tr>
+        </thead>
+        <tbody>
         {contacts.map(({
             id,
             name,
@@ -31,7 +45,8 @@ function ContactList({
                 company={company}
             />
         ))}
-    </div>
+        </tbody>
+    </table>
   );
 }
 export default ContactList;
--- contacts/src/ContactList/components/ListItem/ListItem.tsx
+++ contacts/src/ContactList/components/ListItem/ListItem.tsx
@@ -1,39 +1,34 @@
-import { Address, Company } from "../../../types/contact";
+import {Address, Company} from "../../../types/contact";
 
 interface ListItemProps {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: Address;
-  phone: string;
-  website: string;
-  company: Company;
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
 }
 
 function ListItem({
-  id,
-  name,
-  username,
-  email,
-  address,
-  phone,
-  website,
-  company,
-}: ListItemProps) {
-  return <div>
-    <h1>{name}</h1>
-    <p>{username}</p>
-    <p>{email}</p>
-    <p>{address.street}</p>
-    <p>{address.suite}</p>
-    <p>{address.city}</p>
-    <p>{address.zipcode}</p>
-    <p>{phone}</p>
-    <p>{website}</p>
-    <p>{company.name}</p>
-    <p>{company.catchPhrase}</p>
-    <p>{company.bs}</p>
-  </div>;
+                      id,
+                      name,
+                      username,
+                      email,
+                      address,
+                      phone,
+                      website,
+                      company,
+                  }: ListItemProps) {
+    return <tr
+        role="button"
+        className="hover:bg-[#f5f5f5] group">
+        <td className="gap-5 px-4">{name}
+        </td>
+        <td className="gap-5 px-4">{phone}</td>
+        <td className="gap-5 px-4">{email}</td>
+    </tr>;
 }
+
 export default ListItem;
