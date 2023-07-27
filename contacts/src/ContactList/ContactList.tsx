ec40c5dd1da22ba0e86cdf1e49a27768d992892d
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-27 11:54:23 +1000

    locate spinner to mid of page

--- contacts/src/ContactList/ContactList.tsx
+++ contacts/src/ContactList/ContactList.tsx
@@ -16,58 +16,63 @@ function ContactList() {
                 setTimeout(() => {
                     setLoading(false);
                     setContacts(json);
-                }, 5000);
+                }, 1500);
 
             });
     }, []);
     return (
-        <table className="w-full border-separate">
-            <thead className="top-16 z-10 bg-white border-b">
-            <tr>
-                <th className="w-1/2 text-left font-normal py-3 border-b px-4">
-                    Name
-                </th>
-                <th className="w-1/2 text-left font-normal py-3 border-b px-4">
-                    Phone Number
-                </th>
-                <th className="w-1/2 text-left font-normal py-3 border-b px-4">
-                    Email
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            {loading ? (
-                    <Loading/>
-                ) :
+        <>
+            {loading ? <Loading/> :
+                <table className="w-full border-separate">
+                    <thead className="top-16 z-10 bg-white border-b">
+                    <tr>
+                        <th className="w-1/2 text-left font-normal py-3 border-b px-4">
+                            Name
+                        </th>
+                        <th className="w-1/2 text-left font-normal py-3 border-b px-4">
+                            Phone Number
+                        </th>
+                        <th className="w-1/2 text-left font-normal py-3 border-b px-4">
+                            Email
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {loading ? (
+                            <Loading/>
+                        ) :
 
-                contacts.map(({
-                                  id,
-                                  name,
-                                  username,
-                                  email,
-                                  address,
-                                  phone,
-                                  website,
-                                  company,
-                              }) => (
-                    <ListItem
-                        key={id}
-                        id={id}
-                        name={name}
-                        username={username}
-                        email={email}
-                        address={address}
-                        phone={phone}
-                        website={website}
-                        company={company}
-                    />
-                ))
+                        contacts.map(({
+                                          id,
+                                          name,
+                                          username,
+                                          email,
+                                          address,
+                                          phone,
+                                          website,
+                                          company,
+                                      }) => (
+                            <ListItem
+                                key={id}
+                                id={id}
+                                name={name}
+                                username={username}
+                                email={email}
+                                address={address}
+                                phone={phone}
+                                website={website}
+                                company={company}
+                            />
+                        ))
 
+                    }
+
+                    </tbody>
+
+                </table>
             }
 
-            </tbody>
-
-        </table>
+        </>
     );
 }
 
