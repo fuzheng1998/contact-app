44faf797649a0d1d6c45c6d1632ed259b806437f
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-28 20:18:02 +1000

    clean wrong code

--- contacts/src/ContactList/ContactList.tsx
+++ contacts/src/ContactList/ContactList.tsx
@@ -1,80 +1,73 @@
-import {Contact} from "../types/contact";
+import { Contact } from "../types/contact";
 import ListItem from "./components/ListItem";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Loading from "../components/Loading";
 
 function ContactList() {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [contacts, setContacts] = useState<Array<Contact>>(
-        [] as Array<Contact>
-    );
-    useEffect(() => {
-        setLoading(true)
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((json) => {
-                setTimeout(() => {
-                    setLoading(false);
-                    setContacts(json);
-                }, 1500);
-
-            });
-    }, []);
-    return (
-        <>
-            {loading ? <Loading/> :
-                <table className="w-full border-separate">
-                    <thead className="top-16 z-10 bg-white border-b">
-                    <tr>
-                        <th className="w-1/2 text-left font-semibold py-3 border-b px-4">
-                            Name
-                        </th>
-                        <th className="w-1/2 text-left font-semibold py-3 border-b px-4">
-                            Phone Number
-                        </th>
-                        <th className="w-1/2 text-left font-semibold py-3 border-b px-4">
-                            Email
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {loading ? (
-                            <Loading/>
-                        ) :
-
-                        contacts.map(({
-                                          id,
-                                          name,
-                                          username,
-                                          email,
-                                          address,
-                                          phone,
-                                          website,
-                                          company,
-                                      }) => (
-                            <ListItem
-                                key={id}
-                                id={id}
-                                name={name}
-                                username={username}
-                                email={email}
-                                address={address}
-                                phone={phone}
-                                website={website}
-                                company={company}
-                            />
-                        ))
-
-                    }
-
-                    </tbody>
-
-                </table>
-            }
-
-        </>
-    );
+  const [loading, setLoading] = useState<boolean>(true);
+  const [contacts, setContacts] = useState<Array<Contact>>(
+    [] as Array<Contact>
+  );
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        setTimeout(() => {
+          setLoading(false);
+          setContacts(json);
+        }, 1500);
+      });
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <table className="w-full border-separate">
+          <thead className="top-16 z-10 bg-white border-b">
+            <tr>
+              <th className="w-1/2 text-left font-semibold py-3 border-b px-4">
+                Name
+              </th>
+              <th className="w-1/2 text-left font-semibold py-3 border-b px-4">
+                Phone Number
+              </th>
+              <th className="w-1/2 text-left font-semibold py-3 border-b px-4">
+                Email
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map(
+              ({
+                id,
+                name,
+                username,
+                email,
+                address,
+                phone,
+                website,
+                company,
+              }) => (
+                <ListItem
+                  key={id}
+                  id={id}
+                  name={name}
+                  username={username}
+                  email={email}
+                  address={address}
+                  phone={phone}
+                  website={website}
+                  company={company}
+                />
+              )
+            )}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
 
 export default ContactList;
-    
--- contacts/src/ContactList/components/ListItem/ListItem.tsx
+++ contacts/src/ContactList/components/ListItem/ListItem.tsx
@@ -1,57 +1,56 @@
-import {Address, Company} from "../../../types/contact";
+import { Address, Company } from "../../../types/contact";
 import Dialog from "../Dialog/DetailDialog";
-import {useState} from "react";
+import { useState } from "react";
 import classNames from "classnames";
 
 interface ListItemProps {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    address: Address;
-    phone: string;
-    website: string;
-    company: Company;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
 }
 
 function ListItem({
-                      id,
-                      name,
-                      username,
-                      email,
-                      address,
-                      phone,
-                      website,
-                      company,
-                  }: ListItemProps) {
-// dialog state
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <>
-            <tr
-                role="button"
-                className={classNames("hover:bg-[#f5f5f5] group h-16 cursor-pointer")}
-                onClick={() => setIsOpen(true)}
-            >
-                <td className="gap-5 px-4">{name}
-                </td>
-                <td className="gap-5 px-4">{phone}</td>
-                <td className="gap-5 px-4">{email}</td>
-            </tr>
-            <Dialog isOpen={isOpen} onClose={
-                () => setIsOpen(false)
-            }
-                    id={id}
-                    name={name}
-                    username={username}
-                    email={email}
-                    address={address}
-                    phone={phone}
-                    website={website}
-                    company={company}
-            />
-        </>
-    );
+  id,
+  name,
+  username,
+  email,
+  address,
+  phone,
+  website,
+  company,
+}: ListItemProps) {
+  // dialog state
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <tr
+        role="button"
+        className={classNames("hover:bg-[#f5f5f5] group h-16 cursor-pointer")}
+        onClick={() => setIsOpen(true)}
+      >
+        <td className="gap-5 px-4">{name}</td>
+        <td className="gap-5 px-4">{phone}</td>
+        <td className="gap-5 px-4">{email}</td>
+      </tr>
+      <Dialog
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        id={id}
+        name={name}
+        username={username}
+        email={email}
+        address={address}
+        phone={phone}
+        website={website}
+        company={company}
+      />
+    </>
+  );
 }
 
 export default ListItem;
