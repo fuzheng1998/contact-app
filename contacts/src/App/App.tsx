580b7025fed883ea54063a44a7fa8f65b906c200
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-07-29 11:21:16 +1000

    margin to large device

--- contacts/src/App/App.tsx
+++ contacts/src/App/App.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 function App() {
 
   return (
-    <div className={"flex flex-col"}>
+    <div className={"flex flex-col lg:mx-3"}>
       <Header />
       <ContactList/>
 
